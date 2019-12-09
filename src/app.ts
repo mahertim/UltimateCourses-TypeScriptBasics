860b7c7201007b3cc780aa03192b63980a2c19c6
Tim Maher <tim@timmaher.dev>
2019-12-09 15:44:39 -0500

    The "any" type

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,5 @@
-const pizzas: number = 2;
+let coupon: any;
 
-function offerDiscount(orders: number): boolean {
-  return orders >= 3;
-}
-
-if (offerDiscount(pizzas)) {
-  console.log(`You are entitled to a discount.`);
-} else {
-  console.log(`Order more than 3 pizzas for a discount.`);
-}
+coupon = 25;
+coupon = '25';
+coupon = true;
