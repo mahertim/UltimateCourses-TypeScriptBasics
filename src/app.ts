f137d4e88ddf5aad196fcd0a972cc21ebd6ceca7
Tim Maher <tim@timmaher.dev>
2019-12-09 17:46:04 -0500

    Interfaces and optional properties

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ interface Sizes {
 
 interface Pizza extends Sizes {
   name: string;
+  toppings?: number;
   getAvailableSizes(): string[];
 }
 
@@ -20,5 +21,7 @@ function createPizza(name: string, sizes: string[]): Pizza {
 }
 
 pizza = createPizza('Pepperoni', ['small', 'medium']);
+
+pizza.toppings = 1;
 
 console.log(pizza);
