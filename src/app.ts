0eff76ef48b5bd87b5c34981c4163f947b2c4f70
Tim Maher <tim@timmaher.dev>
2019-12-09 13:41:56 -0500

    Arrow functions and implicit returns

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hey TypeScript!');
+const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+  name: 'Blazzing Inferno',
+  getName: function() {
+    return this.name;
+  },
+};
+
+console.log(pizza.getName());
