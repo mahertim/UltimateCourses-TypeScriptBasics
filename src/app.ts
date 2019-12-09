a309bd0f7cf3e2f4abc87a64350c9884a3209aaf
Tim Maher <tim@timmaher.dev>
2019-12-09 18:32:32 -0500

    Getters and Setters

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
+class Sizes {
+  constructor(public sizes: string[]) {}
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  get availableSizes() {
+    return this.sizes;
+  }
+}
+
+const sizes = new Sizes(['small', 'medium']);
+
+console.log(sizes.availableSizes);
+sizes.availableSizes = ['medium', 'large'];
+console.log(sizes.availableSizes);
+
 class Pizza {
   public toppings: string[] = [];
 
