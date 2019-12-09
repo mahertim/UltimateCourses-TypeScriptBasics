8c74ee03159fcece5effce8bee2cbc842c423ddf
Tim Maher <tim@timmaher.dev>
2019-12-09 14:20:00 -0500

    Array spread operator

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-function sumAll(message: string, ...arr: number[]) {
-  console.log(message);
-  return arr.reduce((prev, next) => prev + next);
-}
+const toppings = ['bacon', 'chilli'];
 
-const sum = sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+const newToppings = ['pepperoni'];
 
-console.log(sum);
+const allToppings = [...newToppings, ...toppings];
+
+console.log(allToppings);
