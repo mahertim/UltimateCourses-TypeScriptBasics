94ae3964618eb3a16ad93ce5cf72823afbfa251e
Tim Maher <tim@timmaher.dev>
2019-12-09 18:57:08 -0500

    Protected members and inheiritance

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 abstract class Sizes {
-  constructor(public sizes: string[]) {}
+  constructor(protected sizes: string[]) {}
 
   set availableSizes(sizes: string[]) {
     this.sizes = sizes;
@@ -13,8 +13,12 @@ abstract class Sizes {
 class Pizza extends Sizes {
   public toppings: string[] = [];
 
-  constructor(readonly name: string, public sizes: string[]) {
+  constructor(readonly name: string, sizes: string[]) {
     super(sizes);
+  }
+
+  public updateSizes(sizes: string[]) {
+    this.sizes = sizes;
   }
 
   public addTopping(topping: string) {
@@ -26,6 +30,6 @@ const pizza = new Pizza('Pepperoni', ['small', 'medium']);
 
 console.log(pizza.availableSizes);
 
-pizza.addTopping('pepperoni');
+pizza.updateSizes(['large']);
 
-console.log(pizza);
+console.log(pizza.availableSizes);
