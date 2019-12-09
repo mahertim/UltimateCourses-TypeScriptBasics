ae6b2dbfb16bec5c502be1ecb34e623919b50375
Tim Maher <tim@timmaher.dev>
2019-12-09 17:29:36 -0500

    Interfaces with function types

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 interface Pizza {
   name: string;
   sizes: string[];
+  getAvailableSizes(): string[];
 }
 
 let pizza: Pizza;
@@ -9,6 +10,9 @@ function createPizza(name: string, sizes: string[]): Pizza {
   return {
     name,
     sizes,
+    getAvailableSizes() {
+      return this.sizes;
+    },
   };
 }
 
