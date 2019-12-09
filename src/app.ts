cf07d13c37a8eddd09ca9681434bc0d1511df86f
Tim Maher <tim@timmaher.dev>
2019-12-09 15:28:00 -0500

    Number types, arguments, and functions

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,10 @@
-const pizza = {
-  name: 'Pepperoni',
-  toppings: ['pepperoni', 'bacon', 'chilli'],
-};
+const pizzaCost: number = 10;
+const pizzaToppings: number = 5;
 
-function order({ name: pizzaName, toppings: pizzaToppings }: any) {
-  return { pizzaName, pizzaToppings };
+function calculatePrice(cost: number, toppings: number): number {
+  return cost + 1.5 * toppings;
 }
 
-const { pizzaToppings } = order(pizza);
+const cost: number = calculatePrice(pizzaCost, pizzaToppings);
 
-const toppings = pizzaToppings;
-
-const [first, second, third] = toppings;
-
-console.log(first, second, third);
-
-function logToppings([first, second, third]: any) {
-  console.log(
-    'Topping 1: ',
-    first,
-    '\nTopping 2: ',
-    second,
-    '\nTopping 3: ',
-    third,
-  );
-}
-
-logToppings(toppings);
+console.log(`Pizza costs: ${cost}`);
